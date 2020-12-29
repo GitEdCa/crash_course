440c25d6934cbd4fe72a7ea43d6c86b4e6ba2e71
Edgar Cabral <edgar.cabral@outlook.com>
2020-12-28 19:38:32 -0600

    Add first five exercises for Chapter 08

---
+++ 08_functions/05_cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country="Mexico"):
+    """This function will print the city and country."""
+    print(city + " is in " + country)
+
+describe_city("Mexico City");
+describe_city(city="Guadalajara")
+describe_city("Monterrey", "Nuevo Leon")
